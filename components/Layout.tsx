c3c18b4d635b6fbd4caa9597d63eab6a5ba912f8
Ivan Garcia <ivanvlora@gmail.com>
2023-02-05 13:26:53 +0900

    [feature] portfolio page

---
+++ components/Layout.tsx
@@ -0,0 +1,58 @@
+import { Navbar, Footer } from "flowbite-react";
+import React, { ReactNode } from "react";
+import styles from "@/styles/Home.module.css";
+
+interface Props {
+  children?: ReactNode;
+}
+
+export default function Home({ children }: Props) {
+  return (
+    <>
+      <Navbar fluid={true} rounded={true}>
+        <Navbar.Brand href="/">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="mr-3 h-6 sm:h-9"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            Flowbite
+          </span>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Navbar.Link href="/" active={true}>
+            Home
+          </Navbar.Link>
+          <Navbar.Link href="/blog">Blog</Navbar.Link>
+          <Navbar.Link href="/portfolio">Portfolio</Navbar.Link>
+          <Navbar.Link href="/contact">Contact</Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <main className={styles.main}>{children}</main>
+
+      <Footer container={true}>
+        <div className="w-full text-center">
+          <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
+            <Footer.Brand
+              href="https://flowbite.com"
+              src="https://flowbite.com/docs/images/logo.svg"
+              alt="Flowbite Logo"
+              name="Flowbite"
+            />
+            <Footer.LinkGroup>
+              <Footer.Link href="#">About</Footer.Link>
+              <Footer.Link href="#">Privacy Policy</Footer.Link>
+              <Footer.Link href="#">Licensing</Footer.Link>
+              <Footer.Link href="#">Contact</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+          <Footer.Divider />
+          <Footer.Copyright href="#" by="Flowbite™" year={2022} />
+        </div>
+      </Footer>
+    </>
+  );
+}
