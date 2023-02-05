0394e17e16b7800f5a7fb52d395768a5eb6f6bc0
Ivan Garcia <ivanvlora@gmail.com>
2023-02-06 00:04:01 +0900

    [bug] remove check for length

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -60,7 +60,7 @@ const Home: NextPageWithLayout = ({ post: { data, errors } }: any) => {
         <div className="error">An error occurred: {errors.message}</div>
       )}
 
-      {!data.length && <div className="error">No posts found</div>}
+      {!data && <div className="error">No posts found</div>}
 
       <header className="format format-sm sm:format-base lg:format-lg dark:format-invert py-10">
         <h1>Blog</h1>
